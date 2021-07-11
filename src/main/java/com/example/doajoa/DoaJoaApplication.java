60bf8e0415c4ae6412a8470d4cff159715c6120d
lavaspoon <lavaspoon@naver.com>
2021-07-11 16:27:12 +0900

    Initial commit

---
+++ src/main/java/com/example/doajoa/DoaJoaApplication.java
@@ -0,0 +1,13 @@
+package com.example.doajoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoaJoaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoaJoaApplication.class, args);
+    }
+
+}
